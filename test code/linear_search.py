baf4a1fdd84cc288440476b7f9e09c5a3dfc3f91
rutujaingole <72335505+rutujaingole@users.noreply.github.com>
2022-06-10 23:01:04 +0530

    Delete linear_search.py

--- test code/linear_search.py
+++
@@ -1,8 +0,0 @@
-def linearSearch(arr, x):
-    pos = -1
-    for i in range(len(arr)):
-        if arr[i] == x:
-            pos = i
-            print("Found at {}".format(pos))
-    if pos == -1:
-        print("Not Found")
